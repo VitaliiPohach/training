3c8c641ea9075d053c79472f45500524006a523e
VitaliiPohach <vitaliypogach@gmail.com>
2020-11-11 15:12:50 +0200

    converted tabs to spaces

--- public/js/myValidate.js
+++ public/js/myValidate.js
@@ -1,31 +1,31 @@
 $('#registrationForm').validate({
-	rules:{
-		telnumber: {
-			required: true
-		},
-		mail :{
-			required: true,
-			email: true
-		},
-		datepicker:{
-			required:true
-		},
-		gender:{
-			required: true
-		}
-	},
-	messages:{
-		telnumber:{
-			required: "Поле обов'язкове для заповнення"
-		},
-		mail:{
-			required: "Поле обов'язкове для заповнення",
-			email: "Eлектронна пошта не коректна"
-		},
-		datepicker:{
-			required:"Поле обов'язкове для заповнення"
-		}
-	}
+    rules:{
+        telnumber: {
+            required: true
+        },
+        mail :{
+            required: true,
+            email: true
+        },
+        datepicker:{
+            required:true
+        },
+        gender:{
+            required: true
+        }
+    },
+    messages:{
+        telnumber:{
+            required: "Поле обов'язкове для заповнення"
+        },
+        mail:{
+            required: "Поле обов'язкове для заповнення",
+            email: "Eлектронна пошта не коректна"
+        },
+        datepicker:{
+            required:"Поле обов'язкове для заповнення"
+        }
+    }
 });
 
 $("#telnumber").inputmask("+38(999) 999-9999");
